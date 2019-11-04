5f7fa7ccd83efe10573e149e436f4952818c6a8f
Adam Wisniewski <adamw@tbcn.ca>
2019-11-03 21:38:48 -0500

    initial

---
+++ app/app.py
@@ -0,0 +1,19 @@
+import sys, time
+from flask import Flask, render_template
+
+print("I'm starting")
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+@app.route("/test")
+def test():
+    return render_template("test.html")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000, debug=True)
+
+    
---
+++ start.sh
@@ -0,0 +1,4 @@
+export FLASK_DEBUG=1
+export FLASK_APP=/app/app.py
+export FLASK_RUN_PORT=8000
+flask run
