29ab9891a53be51bff3fe593e709fbb64080a871
Adam Wisniewski <adamw@tbcn.ca>
2019-11-05 22:29:16 -0500

    presented version

--- app/app.py
+++ app/app.py
@@ -1,7 +1,10 @@
 import sys, time
 from flask import Flask, render_template
 
-print("I'm starting")
+from flask import Flask, render_template
+from flask import send_file
+from io import BytesIO
+import qrcode
 
 app = Flask(__name__)
 
@@ -13,7 +16,12 @@ def hello():
 def test():
     return render_template("test.html")
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000, debug=True)
 
-    
+@app.route("/qrcode")
+def qrcode_render():
+    img = qrcode.make("Adam was")
+
+    img_io = BytesIO()
+    img.save(img_io, "jpeg", quality=70)
+    img_io.seek(0)
+    return send_file(img_io, mimetype='image/jpeg', attachment_filename="qrcode.jpeg")
